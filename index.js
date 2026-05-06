85af7731821c90e04cd6037b479fd3e32cd9bbd0
sttrackstar <sonalt321@gmail.com>
2026-05-06 15:03:54 -0400

    New object-oriented JavaScript file created

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
 //Write your code here
+const attendee = {
+  attendeeID: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00
+};
 
+function logAttendeeName(attendee) {
+  console.log(attendee.name);
+}
+
+function logTicketPrice(attendee) {
+  console.log(attendee.ticketPrice);
+}
+
+function updateTicketType(attendee, newTicketType) {
+  attendee.ticketType = newTicketType;
+}
+
+function updateTicketPrice(attendee, newTicketPrice) {
+  attendee.ticketPrice = newPrice;
+}
+
+function removeEventProperty(attendee) {
+  delete attendee.event;
+}
+
+function addCheckedInProperty(attendee) {
+  attendee.checkedIn = true;
+}
 
 
 //Needed for the tests to work. Don't modify
